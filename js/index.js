9caa073cc4da4992303c1c71114b5f8296389b14
Erica Manning <60369190+ejmanning@users.noreply.github.com>
2021-04-05 18:18:48 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ function reset() {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("../serviceWorker.js")
       .then(res => console.log("Service worker registered"))
       .catch(err => console.log("Service worker not registered", err));
   });
